6d0c77b3f9c3e2ddccca76d83d6649e15069f798
Wrokbrook repo <chukschiboy@gmail.com>
2021-02-12 12:17:35 -0800

    update of the account page

--- lib/pages/account_page.dart
+++ lib/pages/account_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:instagram_app/theme/colors.dart';
+import 'package:instagram_app/util/constant.dart';
 
 class AccountPage extends StatefulWidget {
   @override
@@ -6,12 +9,329 @@ class AccountPage extends StatefulWidget {
 }
 
 class _AccountPageState extends State<AccountPage> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
     return Scaffold(
-      body: Center(
-        child: Text("Account"),
+      appBar: getAppBar(),
+      body: getBody(size),
+    );
+  }
+
+  Widget getAppBar() {
+    return PreferredSize(
+      preferredSize: Size.fromHeight(55),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 10.0,
+            right: 10.0,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    Feather.lock,
+                    size: 18.0,
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    username,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    splashRadius: 15.0,
+                    icon: Icon(AntDesign.plus),
+                    onPressed: () {},
+                  ),
+                  IconButton(
+                    splashRadius: 15.0,
+                    icon: Icon(FontAwesome.bars),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
+    );
+  }
+
+  Widget getBody(size) {
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: (size.width - 20) * 0.3,
+                    child: Stack(
+                      children: [
+                        Container(
+                          height: 100.0,
+                          width: 100.0,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 1.0,
+                              color: bgGrey,
+                            ),
+                            image: DecorationImage(
+                              image: NetworkImage(profile),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          right: 25.0,
+                          child: Container(
+                            height: 25.0,
+                            width: 25.0,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: primary,
+                              border: Border.all(
+                                width: 1.0,
+                                color: bgWhite,
+                              ),
+                            ),
+                            child: Center(
+                              child: Icon(
+                                Icons.add,
+                                color: bgWhite,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: (size.width - 20) * 0.7,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "61",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Posts",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                                height: 1.5,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "117",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Followers",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                                height: 1.5,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "173",
+                              style: TextStyle(
+                                fontSize: 18.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Following",
+                              style: TextStyle(
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.bold,
+                                height: 1.5,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Text(instagramName),
+              Text(instagramBio),
+              SizedBox(
+                height: 15.0,
+              ),
+              Container(
+                height: 35.0,
+                width: (size.width - 20),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1.0,
+                    color: bgGrey,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                  color: bgLightGrey,
+                ),
+                child: Center(
+                  child: Text(
+                    "Edit Profile",
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Story Highlights",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Icon(
+                    FontAwesome.angle_down,
+                    size: 20.0,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 15.0,
+        ),
+        Container(
+          height: 0.5,
+          width: size.width,
+          decoration: BoxDecoration(
+            color: bgGrey.withOpacity(0.8),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(
+            vertical: 3.0,
+          ),
+          child: Row(
+            children: [
+              Container(
+                width: (size.width * 0.5),
+                child: IconButton(
+                  splashRadius: 20.0,
+                  icon: Icon(
+                    FontAwesome.th,
+                    color: selectedIndex == 0
+                        ? textBlack
+                        : textBlack.withOpacity(0.5),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      selectedIndex = 0;
+                    });
+                  },
+                ),
+              ),
+              Container(
+                width: (size.width * 0.5),
+                child: IconButton(
+                  splashRadius: 20.0,
+                  icon: Icon(
+                    FontAwesome.id_badge,
+                    color: selectedIndex == 1
+                        ? textBlack
+                        : textBlack.withOpacity(0.5),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      selectedIndex = 1;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        Column(
+          children: [
+            Row(
+              children: [
+                Container(
+                  height: 1.0,
+                  width: (size.width * 0.5),
+                  decoration: BoxDecoration(
+                    color: selectedIndex == 0 ? bgDark : Colors.transparent,
+                  ),
+                ),
+                Container(
+                  height: 1.0,
+                  width: (size.width * 0.5),
+                  decoration: BoxDecoration(
+                    color: selectedIndex == 1 ? bgDark : Colors.transparent,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 0.5,
+              width: size.width,
+              decoration: BoxDecoration(
+                color: bgGrey.withOpacity(0.8),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 3.0,
+        ),
+        IndexedStack(
+          index: selectedIndex,
+          children: [
+            Text("Hello"),
+            Text("Hi"),
+          ],
+        ),
+      ],
     );
   }
 }
